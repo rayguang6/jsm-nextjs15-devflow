823681171dd0c165c3a50ba005c3e8be679b0968
Lei Zhi Guang <rayguang123@gmail.com>
2025-05-07 02:14:36 +0800

    Implement Accounts with oAuth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 import { api } from "@/lib/api";
 import handleError from "@/lib/handlers/error";
+import { auth } from "@/auth";
 
 const questions = [
   {
@@ -49,22 +50,15 @@ const questions = [
   },
 ];
 
-const test = async () => {
-  try {
-    return await api.users.getAll();
-  } catch (error) {
-    return handleError(error);
-  }
-};
 
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
-  const users = await test();
+  const session = await auth();
 
-  console.log(users);
+  console.log("Session", session);
 
   const { query = "", filter = "" } = await searchParams;
 
