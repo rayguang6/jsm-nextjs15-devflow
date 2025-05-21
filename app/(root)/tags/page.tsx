ce546ac38267a67cc8452beb438e85e16e83cc12
Lei Zhi Guang <rayguang123@gmail.com>
2025-05-21 12:58:14 +0800

    Implement getTags server action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
- 
- const Tags = () => {
-   return <div>Tags</div>;
- };
- 
- export default Tags;
+import { getTags } from "@/lib/actions/tag.actions";
+
+const Tags = async () => {
+  const { success, data, error } = await getTags({
+    page: 1,
+    pageSize: 10,
+    query: "javascript",
+  });
+
+  const { tags } = data || {};
+
+  console.log("TAGS", JSON.stringify(tags, null, 2));
+
+  return <div>Tags</div>;
+};
+
+export default Tags;
