63649cb95e201a76ee2683f2ad678c63ea795291
Lei Zhi Guang <rayguang123@gmail.com>
2025-05-21 14:13:26 +0800

    Increment Question Details Views

---
+++ app/(root)/questions/view.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { toast } from "sonner";
+import { incrementViews } from "@/lib/actions/question.action";
+import { useEffect } from "react";
+
+const View = ({ questionId }: { questionId: string }) => {
+  const handleIncrement = async () => {
+    const result = await incrementViews({ questionId });
+
+    if (result.success) {
+      toast.success("Views incremented");
+    } else {
+      toast.error(result.error?.message);
+    }
+  };
+
+  useEffect(() => {
+    handleIncrement();
+  }, []);
+
+  return null;
+};
+
+export default View;
