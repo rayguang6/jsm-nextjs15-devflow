6161ca0aeb14e3df03917e03c9e05bfdc032980c
Lei Zhi Guang <rayguang123@gmail.com>
2025-05-24 14:17:11 +0800

    dynamic metadata, and cache at getquestion

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,29 +1,32 @@
+import { Metadata } from "next";
 import Link from "next/link";
 
 import QuestionCard from "@/components/cards/QuestionCard";
+import DataRenderer from "@/components/DataRenderer";
+import CommonFilter from "@/components/filters/CommonFilter";
 import HomeFilter from "@/components/filters/HomeFilter";
+import Pagination from "@/components/Pagination";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
-import { getQuestions } from "@/lib/actions/question.action";
-import DataRenderer from "@/components/DataRenderer";
-import { EMPTY_QUESTION } from "@/constants/states";
-import CommonFilter from "@/components/filters/CommonFilter";
 import { HomePageFilters } from "@/constants/filters";
-import Pagination from "@/components/Pagination";
+import ROUTES from "@/constants/routes";
+import { EMPTY_QUESTION } from "@/constants/states";
+import { getQuestions } from "@/lib/actions/question.action";
 
-interface SearchParams {
-  searchParams: Promise<{ [key: string]: string }>;
-}
+export const metadata: Metadata = {
+  title: "Dev Overflow | Home",
+  description:
+    "Discover different programming questions and answers with recommendations from the community.",
+};
 
-const Home = async ({ searchParams }: SearchParams) => {
+async function Home({ searchParams }: RouteParams) {
   const { page, pageSize, query, filter } = await searchParams;
 
   const { success, data, error } = await getQuestions({
     page: Number(page) || 1,
     pageSize: Number(pageSize) || 10,
-    query: query || "",
-    filter: filter || "",
+    query,
+    filter,
   });
 
   const { questions, isNext } = data || {};
@@ -32,28 +35,32 @@ const Home = async ({ searchParams }: SearchParams) => {
     <>
       <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
-
         <Button
           className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
           asChild
         >
-          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+          <Link href={ROUTES.ASK_QUESTION} className="max-sm:w-full">
+            Ask a Question
+          </Link>
         </Button>
       </section>
-      <section className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
 
+      <section className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearch
-          route="/"
+          route={ROUTES.HOME}
           imgSrc="/icons/search.svg"
           placeholder="Search questions..."
+          iconPosition="left"
           otherClasses="flex-1"
         />
+
         <CommonFilter
           filters={HomePageFilters}
           otherClasses="min-h-[56px] sm:min-w-[170px]"
           containerClasses="hidden max-md:flex"
         />
       </section>
+
       <HomeFilter />
 
       <DataRenderer
@@ -69,10 +76,10 @@ const Home = async ({ searchParams }: SearchParams) => {
           </div>
         )}
       />
-      <Pagination page={page} isNext={isNext || false} />
 
+      <Pagination page={page} isNext={isNext || false} />
     </>
   );
-};
+}
 
 export default Home;
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -22,7 +22,7 @@ import {
 
 import dbConnect from "../mongoose";
 import { createInteraction } from "./interaction.action";
-
+import { cache } from "react";
 export async function createQuestion(
   params: CreateQuestionParams
 ): Promise<ActionResponse<Question>> {
@@ -200,7 +200,8 @@ export async function editQuestion(
   }
 }
 
-export async function getQuestion(
+export const getQuestion = cache(async function getQuestion(
+
   params: GetQuestionParams
 ): Promise<ActionResponse<Question>> {
   const validationResult = await action({
@@ -225,7 +226,8 @@ export async function getQuestion(
   } catch (error) {
     return handleError(error) as ErrorResponse;
   }
-}
+});
+
 
 export async function getRecommendedQuestions({
   userId,
