c08435ed5434f1b18e92676f6d9f69b869a01fcd
Lei Zhi Guang <rayguang123@gmail.com>
2025-05-24 08:32:09 +0800

    Implement User's Questions

--- types/action.d.ts
+++ types/action.d.ts
@@ -72,3 +72,9 @@ interface CollectionBaseParams {
 interface GetUserParams {
   userId: string;
 }
+
+
+interface GetUserQuestionsParams
+  extends Omit<PaginatedSearchParams, "query" | "filter" | "sort"> {
+  userId: string;
+}
